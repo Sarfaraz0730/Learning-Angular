784af89e3617e679650b2bf0ab948efc85c2503c
sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-05-15 23:07:03 +0530

    day4

--- Day3/helloworld/src/app/app.component.ts
+++ Day3/helloworld/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="border: 2px solid red;">
+      <h1>Welcome to your life</h1>
+      <hr>
+      <vijay-comp></vijay-comp>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'helloworld';
-}
+  title = 'helloworld'}
--- Day3/helloworld/src/app/app.module.ts
+++ Day3/helloworld/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+ 
 import { AppComponent } from './app.component';
-
+import { SecondComp } from './second.component';
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  /* declarations is list of all components you can use in your application */
+  declarations: [ AppComponent, SecondComp ],
+  /* imports is all modules you are using in your application */
+  imports: [ BrowserModule ],
+  /* providers is a list of services that are injected to your application */
   providers: [],
-  bootstrap: [AppComponent]
+  /* bootstrap is a list of main components of your application */
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { SecondComp } from './second.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent, SecondComp
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent, SecondComp]
+// })
+// export class AppModule { }
