1cf73e244eda1c757d72f33852ce3af2ea85af46
sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-05-16 19:18:51 +0530

    p

--- Day4/learning-project/src/app/app.component.ts
+++ Day4/learning-project/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-project';
+  getName(name:any){
+    alert(name)
+  };
+  functionTwo(){
+    alert("Function two is calling")
+  }
 }
