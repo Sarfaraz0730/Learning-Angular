df30618ebd32575fcacf623889c37393cb357654
sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-05-16 18:40:05 +0530

    practice

---
+++ Day4/learning-project/src/app/student-list/student-list.component.ts
@@ -0,0 +1,16 @@
+import { Component , OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-student-list',
+  template: `
+  <h2 class="custom">
+  <p >
+      student-list works!
+    </p>
+  </h2>
+  `,
+  styleUrls: ['./student-list.component.css']
+})
+export class StudentListComponent {
+
+}
